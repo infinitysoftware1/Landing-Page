080d82d4ca2e980b15fc429a2b29e1157b8e9d88
Infinite Software <infinitesoftware01@gmail.com>
2024-03-26 20:32:20 -0300

    Atualização

--- Script.js
+++ Script.js
@@ -2,5 +2,10 @@ function scrollToDiv(targetId) {
     let targetDiv = document.getElementById(targetId);
     targetDiv.scrollIntoView({ behavior: 'smooth'});
 };
+
+function redirecionarParaGmail() {
+    window.open('https://mail.google.com/mail/u/0/?pli=1#inbox?compose=CllgCJvpbTBQPdmckNJKdvwNRhndwTwmTSPCdgKgFQxnSWGGmcCNRhspqpVKmbcCphkrTxDNTLB', '_blank');
+}
+
   
   
