9e835deb5aee45c9cbd4ebfb679db5e8906fcd7a
Infinite Software <infinitesoftware01@gmail.com>
2024-03-26 12:51:10 -0300

    Atualização

--- Script.js
+++ Script.js
@@ -1,37 +1,6 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
-
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector(".carousel");
-    const cards = document.querySelectorAll(".card");
-    const arrowNext = document.querySelector(".arrow-next");
-    const arrowPrev = document.querySelector(".arrow-prev");
-  
-    let currentIndex = 0;
-    const cardWidth = cards[0].offsetWidth + parseInt(window.getComputedStyle(cards[0]).marginRight);
-  
-    arrowNext.addEventListener("click", function() {
-      if (currentIndex < cards.length - 1) {
-        currentIndex++;
-        scrollToCurrentIndex();
-      }
-    });
-  
-    arrowPrev.addEventListener("click", function() {
-      if (currentIndex > 0) {
-        currentIndex--;
-        scrollToCurrentIndex();
-      }
-    });
-  
-    function scrollToCurrentIndex() {
-      carousel.scrollTo({
-        left: currentIndex * cardWidth,
-        behavior: "smooth"
-      });
-    }
-  });
+function scrollToDiv(targetId) {
+    let targetDiv = document.getElementById(targetId);
+    targetDiv.scrollIntoView({ behavior: 'smooth'});
+};
   
   
